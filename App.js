ffea01cdbd4d21f9010ec10d2dff554b91be0427
dev-DanielSilva <dev-DanielSilva@outlook.com>
2025-03-26 15:24:23 -0300

    enviado

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
+
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
+    // Fazer tela splash com o ícone
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Página Inicial</Text>
+{/* Tela login  */}
+<Text>Ainda não possui uma conta</Text>
+{/* Leva para tela de registro */}
     </View>
   );
 }
