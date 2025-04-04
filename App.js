8a3084dec518c247bec5f9191f4eca61ed979387
lviz-tk <luizcarlosrealgames@gmail.com>
2025-04-04 11:16:59 -0300

    TCC 100% atualizado

--- App.js
+++ App.js
@@ -1,35 +1,37 @@
 import React from 'react';
-
 import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack'
+import { NavigationContainer} from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Registro from './src/telas/Registro';
-import Post from './src/telas/Post';
-import feed from './src/telas/feed';
-import perfil from './src/telas/perfil';
 
-import {Ionicons} from '@expo/vector-icons'
-import { TextInput } from 'react-native-web';
-const Tab = createBottomTabNavigator();
+
+
+
+
+import Login from './src/telas/Login';
+
+
+import {Ionicons} from '@expo/vector-icons';
+
+const Tab= createBottomTabNavigator();
 
 function Tabs(){
-
-   return(
- <Tab.Navigator
+  return (
+    <Tab.Navigator
     screenOptions={({ route }) => ({
       tabBarIcon: ({ focused, color, size }) => {
         let iconName;
-        color= '#d0d'
-        size = 30
-        if (route.name === 'Post') {
+        if (route.name === 'Produtos') {
           iconName = focused
-            ? 'home-outline'
-            : 'home-outline';
-        } else if (route.name === 'historico') {
-          iconName = focused ? 'person-outline' : 'person-outline';
-        }else if (route.name === 'formapagamento') {
-          iconName = focused ? 'people-outline' : 'people-outline';
+            ? 'home'
+            : 'home';
+        } else if (route.name === 'Produtos') {
+          iconName = focused ? 'list' : 'list';
+        }else if (route.name === 'Login') {
+          iconName = focused ? 'people' : 'people';
+        }
+        else if (route.name === 'Cadastro'){
+          iconName = focused ? 'albums-outline' : 'albums-outline';
         }
         
         //aqui define os ícones que irão aparecer nas Tabs
@@ -37,55 +39,35 @@ function Tabs(){
       },
     })}
     tabBarOptions={{
-      labelStyle: {
-        fontSize: 12},
       activeTintColor: '#3f64c7',
       inactiveTintColor: 'gray',      
     }}    
-    >
-      <Tab.Screen name= "historico" component={historico}></Tab.Screen>
-      <Tab.Screen name= "formapagamento" component={formapagemento}></Tab.Screen>
+    >  
+      <Tab.Screen name= "Login" component={Login}></Tab.Screen>
+      {/* <Tab.Screen name= "Produtos" component={Produtos}></Tab.Screen>
+      <Tab.Screen name= "Cadastro" component={Cadastro}></Tab.Screen> */}
     </Tab.Navigator>
-   )
+  );
 }
 
 export default function App() {
 
-   
-  const Stack = createStackNavigator();
-
-
+const Stack= createStackNavigator();
   return (
-    <NavigationContainer>
-      <Text>Email:</Text>
-      <TextInput></TextInput>
-      <Text>Senha:</Text>
-      <TextInput></TextInput>
-      <Text>CPF:</Text>
-      <TextInput></TextInput>
-      <Text>Data de nascimento:</Text>
-      <TextInput></TextInput>
-    
+  <NavigationContainer>
+    <Stack.Navigator initialRouteName='Login'>
 
-    <Stack.Navigator initialRouteName='Post'>
-
+    <Stack.Screen name="Login" component={Login} options={{headerShown: false}}></Stack.Screen>
       <Stack.Screen 
-          name="historico" 
+          name="Produtos" 
           component={Tabs}
-          options={{
-            title:'Meu Aplicativo',
-            headerStyle:{
-            backgroundColor: '#D80303',
-            },
-            headerTintColor: '#FFF' , 
-            headerShown: true         
-          }}
+          options={{headerShown: false}}
           >
 
       </Stack.Screen>
-      <Stack.Screen name="Post" component={Post} options={{headerShown: false}}></Stack.Screen>
-      <Stack.Screen name="feed" component={feed} ></Stack.Screen>      
-      <Stack.Screen name="historico" component={historico} ></Stack.Screen>
+      {/* <Stack.Screen name ="Cadastro" component={Cadastro} options={{headerShown: false}}></Stack.Screen> */}
+
+
     </Stack.Navigator>
   </NavigationContainer>
   );
