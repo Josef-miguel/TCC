8f976a1787570285b08085ddb5f7229339b685f0
MiguelSouza2 <jubscrebis@gmail.com>
2025-04-11 13:41:51 -0300

    aa

--- App.js
+++ App.js
@@ -11,10 +11,10 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Login from './src/telas/Login';
 import Cadastro from './src/telas/Cadastro'
 import Home from './src/telas/Home'
-import agenda from './src/telas/agenda'
-import formapagamento from './src/telas/formapagamento'
-import historico from './src/telas/historico'
-import perfil from './src/telas/perfil'
+import Agenda from './src/telas/Agenda'
+import Formapagamento from './src/telas/Formapagamento'
+import Historico from './src/telas/Historico'
+import Perfil from './src/telas/Perfil'
 import Post from './src/telas/Post'
 import {Ionicons} from '@expo/vector-icons';
 
@@ -52,10 +52,10 @@ function Tabs(){
       <Tab.Screen name= "Login" component={Login}></Tab.Screen>
       <Tab.Screen name= "Cadastro" component={Cadastro}></Tab.Screen>
        <Tab.Screen name= "Home"component={Home}></Tab.Screen> 
-       <Tab.Screen name= "agenda"  component={agenda}></Tab.Screen> 
-       <Tab.Screen name= "formapagamento" component={formapagamento}></Tab.Screen> 
-       <Tab.Screen name= "historico" component={historico}></Tab.Screen> 
-       <Tab.Screen name= "perfil" component={perfil}></Tab.Screen> 
+       <Tab.Screen name= "Agenda"  component={Agenda}></Tab.Screen> 
+       <Tab.Screen name= "Formapagamento" component={Formapagamento}></Tab.Screen> 
+       <Tab.Screen name= "Historico" component={Historico}></Tab.Screen> 
+       <Tab.Screen name= "Perfil" component={Perfil}></Tab.Screen> 
        <Tab.Screen name= "Post" component={Post}></Tab.Screen> 
 
       {/* <Tab.Screen name= "Produtos" component={Produtos}></Tab.Screen>
@@ -81,14 +81,12 @@ const Stack= createStackNavigator();
       </Stack.Screen>
        <Stack.Screen name ="Cadastro" component={Cadastro} options={{headerShown: false}}></Stack.Screen> 
         <Stack.Screen name ="Home" component={Home} options={{headerShown: false}}></Stack.Screen>  
-        <Stack.Screen name ="agenda" component={agenda} options={{headerShown: false}}></Stack.Screen>  
-        <Stack.Screen name ="formapagamento" component={formapagamento} options={{headerShown: false}}></Stack.Screen>  
-        <Stack.Screen name ="historico" component={historico} options={{headerShown: false}}></Stack.Screen>  
-        <Stack.Screen name ="perfil" component={perfil} options={{headerShown: false}}></Stack.Screen>  
+        <Stack.Screen name ="Agenda" component={Agenda} options={{headerShown: false}}></Stack.Screen>  
+        <Stack.Screen name ="Formapagamento" component={Formapagamento} options={{headerShown: false}}></Stack.Screen>  
+        <Stack.Screen name ="Historico" component={Historico} options={{headerShown: false}}></Stack.Screen>  
+        <Stack.Screen name ="Perfil" component={Perfil} options={{headerShown: false}}></Stack.Screen>  
         <Stack.Screen name ="Post" component={Post} options={{headerShown: false}}></Stack.Screen>  
 
-
-{/* miguel */}
 
     </Stack.Navigator>
   </NavigationContainer>
