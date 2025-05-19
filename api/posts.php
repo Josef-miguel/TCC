60744c671f6632f1377faa81982c63262bdf9e37
MiguelSouza2 <jubscrebis@gmail.com>
2025-05-19 14:41:57 -0300

    19/05

--- api/posts.php
+++ api/posts.php
@@ -1,43 +1,45 @@
-<?php
+<?php 
 
-include_once("connection.php");
+include_once('connection.php');
 
-header('Content-Type: application/json'); // Importante para o React Native entender o retorno
+$postjson = json_decode(file_get_contents('php://input'), true);
 
-$res = $pdo->prepare("SELECT * FROM evento");
-$res->execute();
 
-$data = $res->fetchAll(PDO::FETCH_ASSOC);
+$query = $pdo->prepare("SELECT * from evento");
 
-if ($data) {
-    $eventos = array_map(function ($evento) {
-        return array(
-            'id' => (int) $evento['id_evento'],
-            'title' => $evento['titulo'],
-            'description' => $evento['descricao'],
-            'route' => $evento['destino'],
-            'route_exit' => $evento['local_saida'],
-            'images' => [ $evento['imagens'] ], // em array!
-            'numSlots' => (int) $evento['n_vagas'],
-            'price' => (float) $evento['preco'],
-            'exit_date' => $evento['data_de_saida'],
-            'return_date' => $evento['data_de_retorno'],
-            'review' => (float) $evento['avaliacao'],
-            'fav' => false, // valor padrão para frontend controlar
-            'theme' => '',  // campos que você pode completar se quiser
-            'type' => ''
-        );
-    }, $data);
+$query->execute();
 
-    echo json_encode([
-        'message' => 'Posts carregados com sucesso!',
-        'success' => true,
-        'data' => $eventos
-    ]);
-} else {
-    echo json_encode([
-        'message' => 'Nenhum evento encontrado!',
-        'success' => false,
-        'data' => []
-    ]);
-}
+$res = $query->fetchAll(PDO::FETCH_ASSOC);
+
+for ($i=0; $i < count($res); $i++) { 
+    foreach ($res[$i] as $key => $value) {  }      
+
+    $dados[] = array(
+        'id_evento' => $res[$i]['id_evento'],  
+        'route' => $res[$i]['destino'],
+        'route_exit' => $res[$i]['local_saida'],
+        'images' => [ $res[$i]['imagens'] ], 
+        'numSlots' => (int) $res[$i]['n_vagas'],
+        'price' => (float) $res[$i]['preco'],
+        'exit_date' => $res[$i]['data_de_saida'],
+        'return_date' => $res[$i]['data_de_retorno'],
+        'review' => (float) $res[$i]['avaliacao'],
+        'fav' => false, // valor padrão para frontend controlar
+        'theme' => '',  // campos que você pode completar se quiser
+        'type' => ''
+                         
+    );
+
+    }
+
+   if(count($res) > 0){
+           $result = json_encode(array('success'=>true, 'result'=>$dados));
+
+       }else{
+           $result = json_encode(array('success'=>false, 'result'=>'0'));
+
+       }
+
+echo $result;
+
+?>
